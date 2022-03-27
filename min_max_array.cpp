206b7ac87065692cec62f265c8086ed62b088bf7
KUNJAL SARMA <62415937+KunjalSarma@users.noreply.github.com>
2022-03-27 09:40:49 +0530

    Add files via upload

---
+++ min_max_array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int getMax(int num[],int n){
+	int max = INT_MIN;
+	for (int i=0;i<n;i++){
+		if(num[i]>max){
+			max = num[i];
+		}
+	}
+	return max;
+}
+
+int getMin(int num[],int n){
+	int min = INT_MAX;
+	for (int i=0;i<n;i++){
+		if(num[i]<min){
+			min = num[i];
+		}
+	}
+		return min;
+	
+}
+
+int main(){
+	//min and max array
+	cout<<"enter the array";
+	int size;
+	cin>>size;
+	//bad practice
+	//int num[size]
+	
+	int num[100];
+	for (int i=0; i<size;i++){
+		cin>>num[i];
+		
+	}
+	cout<<"max value of array  "<<getMax(num,5)<<endl;
+	cout<<"min value of array  "<<getMin(num,5)<<endl;
+	
+	
+	
+	
+	
+}
